0a9ae855ea97ea4bb86e11dc6be1d4e9f0230d30
Tens1des <romankotov3213@gmail.com>
2025-09-22 15:07:56 +0300

    Initial Commit

---
+++ CryptoLite/CryptoLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoLiteApp.swift
+//  CryptoLite
+//
+//  Created by Рома Котов on 22.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
